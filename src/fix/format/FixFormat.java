b5639398cf76dc3887ea945b7c3fab4e5cb8eaaf
Jaspreet Singh <Dell@192.168.2.16>
2017-10-18 19:23:45 -0400

    if the string ends with a dot then it should capitilize the next first letter

--- src/fix/format/FixFormat.java
+++ src/fix/format/FixFormat.java
@@ -5,16 +5,49 @@
  */
 package fix.format;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *
  * @author Dell
  */
 public class FixFormat {
 
- 
-    public static void main(String[] args) {
-       
-        
+    public static void main(String[] args) throws Exception {
+
+        try {
+            FileReader f = new FileReader("C:\\Downloads\\Input.txt");
+            BufferedReader br = new BufferedReader(f);
+            Scanner scfile = new Scanner(br);
+            boolean capitilize = false;
+
+            while (scfile.hasNext()) {  //Is there more words ?
+                String currentWord = scfile.next();     //If yes add the word inside variable
+
+
+                //Print Word
+                if (capitilize) {
+                    System.out.print(currentWord.substring(0, 1).toUpperCase());
+                    System.out.print(currentWord.substring(1));
+
+                    capitilize = false;
+                } else {
+                    System.out.print(currentWord);
+                }
+
+                System.out.print(" ");
+
+                //Next word is it capitilized ?
+                if (currentWord.endsWith(".")) {
+                    capitilize = true;
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.print(" pokk");
+        }
+
     }
-    
-}
